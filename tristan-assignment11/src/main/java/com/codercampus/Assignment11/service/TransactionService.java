72e6ec0b8b5ed7f7950413b383fe2aaee35493e7
tristandanle <letristandan@gmail.com>
2022-04-21 21:05:52 -0400

    Update TransactionService.java

--- tristan-assignment11/src/main/java/com/codercampus/Assignment11/service/TransactionService.java
+++ tristan-assignment11/src/main/java/com/codercampus/Assignment11/service/TransactionService.java
@@ -12,25 +12,35 @@ import com.codercampus.Assignment11.repository.TransactionRepository;
 
 @Service
 public class TransactionService {
+	
+	@Autowired
+	private TransactionRepository transactionRepo;
 
-  @Autowired
-  private TransactionRepository transactionRepo;
+	public List<Transaction> findAll() {
+		List<Transaction> sortedTransaction = 
+				transactionRepo.findAll()
+				.stream()
+				.sorted(Comparator.comparing(Transaction::getDate))
+				.collect(Collectors.toList());
+		
+		return sortedTransaction;
+	}
+	
 
-  public List < Transaction > findAll() {
-    List < Transaction > sortedTransaction = transactionRepo.findAll().stream().sorted(Comparator.comparing(Transaction::getDate)).collect(Collectors.toList());
+	
+	public  Transaction findById(Long transactionId) {
+		
+		return transactionRepo.findAll()
+		.stream()
+		.filter(id -> id.getId().equals(transactionId))
+		.findFirst()
+		.orElse(null);
+	}
+//		for (Transaction transaction:transactionRepo.findAll()) {
+//			if(transaction.getId().equals(transactionId)) {
+//				return  transaction;
+//			}
+//		}
+//		return null;
 
-    return sortedTransaction;
-  }
-
-  public Transaction findById(Long transactionId) {
-
-    return transactionRepo.findAll().stream().filter(id - >id.getId().equals(transactionId)).findFirst().orElse(null);
-  }
-
-  //		for (Transaction transaction:transactionRepo.findAll()) {
-  //			if(transaction.getId().equals(transactionId)) {
-  //				return  transaction;
-  //			}
-  //		}
-  //		return null;
 }
