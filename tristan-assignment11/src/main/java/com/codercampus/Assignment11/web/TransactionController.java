38db355fb0d151008bc19cbd4aa796b67adb7520
tristandanle <letristandan@gmail.com>
2022-04-21 20:51:15 -0400

    Update TransactionController.java

--- tristan-assignment11/src/main/java/com/codercampus/Assignment11/web/TransactionController.java
+++ tristan-assignment11/src/main/java/com/codercampus/Assignment11/web/TransactionController.java
@@ -14,28 +14,28 @@ import com.codercampus.Assignment11.service.TransactionService;
 @Controller
 public class TransactionController {
 
-	@Autowired
-	private TransactionService transactionService;
-	
-	// Fetch transactions from repository to HTML view 
-	@GetMapping("/transactions")
-	public String getTransactions(ModelMap model) {
-		
-		List<Transaction> allTransactions = transactionService.findAll();
-		//Transaction transaction = new Transaction();
-		//model.put("transaction", transaction);
-		model.put("transactions", allTransactions); // all transactions
-		for ( Transaction transaction : allTransactions) {
-			System.out.println(transaction);
-		}
-		return "transactions";
-	}
-	
-	@GetMapping("/transactions/{transactionId}")
-	public String getTransaction(@PathVariable Long transactionId, ModelMap model) {
-		
-		Transaction transactionById = transactionService.findById(transactionId);
-		model.put("transaction",transactionById);
-		return "transactionbyid";
-	}
+  @Autowired
+  private TransactionService transactionService;
+
+  // Fetch transactions from repository to HTML view 
+  @GetMapping("/transactions")
+  public String getTransactions(ModelMap model) {
+
+    List < Transaction > allTransactions = transactionService.findAll();
+    //Transaction transaction = new Transaction();
+    //model.put("transaction", transaction);
+    model.put("transactions", allTransactions); // all transactions
+    for (Transaction transaction: allTransactions) {
+      System.out.println(transaction);
+    }
+    return "transactions";
+  }
+
+  @GetMapping("/transactions/{transactionId}")
+  public String getTransaction(@PathVariable Long transactionId, ModelMap model) {
+
+    Transaction transactionById = transactionService.findById(transactionId);
+    model.put("transaction", transactionById);
+    return "transactionbyid";
+  }
 }
